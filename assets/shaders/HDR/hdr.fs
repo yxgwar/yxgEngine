3fd6a19a0eb773fdd26665076385d5e42d8a3104
yxg <1827463397@qq.com>
2025-02-22 17:41:20 +0800

    加入gamma矫正

--- assets/shaders/HDR/hdr.fs
+++ assets/shaders/HDR/hdr.fs
@@ -8,9 +8,11 @@ uniform sampler2D bloomBlur;
 uniform float exposure;
 void main()
 {
+    const float gamma = 2.2;
     vec3 hdrColor = texture(scene, TexCoords).rgb;      
     vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
     hdrColor += bloomColor; // additive blending
     vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
+    result = pow(result, vec3(1.0 / gamma));
     FragColor = vec4(result, 1.0);
 } 
--- src/renderer/texture.cpp
+++ src/renderer/texture.cpp
@@ -41,9 +41,9 @@ void Texture::load(const char *path)
     if (data)
     {
         if(nrChannels == 4)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB_ALPHA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         else
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
